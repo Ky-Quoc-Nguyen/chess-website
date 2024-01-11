693c62d46cc23dff8f5fe7e1dd19fcb78f34065a
Ky Nguyen <kqn5tv@virginia.edu>
2024-01-11 13:10:54 -0500

    formatted

--- sqlserver.js
+++ sqlserver.js
@@ -1,68 +1,70 @@
-const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
 const app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 app.use(express.json());
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'sqluser', 
-    password: 'password', 
-    database: 'chessClubDB' 
+  host: "localhost",
+  user: "sqluser",
+  password: "password",
+  database: "chessClubDB",
 });
 
-db.connect(err => {
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+    return;
+  }
+  console.log("Connected to the database");
+});
+
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  const sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+  db.query(sql, [username, password], (err, result) => {
     if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
+      console.error("Database error:", err);
+      res.status(500).send("Error registering new user");
+    } else {
+      res.redirect("http://127.0.0.1:5500/index.html#");
     }
-    console.log('Connected to the database');
+  });
 });
 
-app.post('/register', (req, res) => {
-    const { username, password } = req.body;
-    const sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-    db.query(sql, [username, password], (err, result) => {
-        if (err) {
-            console.error('Database error:', err);
-            res.status(500).send('Error registering new user');
-        } else {
-            res.redirect('http://127.0.0.1:5500/index.html#')
-        }
-    });
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  console.log("Received credentials:", username, password);
+
+  const sql = "SELECT * FROM users WHERE username = ?";
+  db.query(sql, [username], (err, results) => {
+    console.log("Database query results:", results);
+    if (err) {
+      console.error("Database error:", err);
+      return res.status(500).send("Internal Server Error");
+    }
+
+    if (results.length > 0) {
+      if (results[0].password === password) {
+        res.json({
+          success: true,
+          message: "Login successful",
+          username: username,
+        });
+      } else {
+        res.json({ success: false, message: "Invalid username or password" });
+      }
+    } else {
+      res.json({ success: false, message: "Invalid username or password" });
+    }
+  });
 });
-
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    console.log("Received credentials:", username, password);
-
-    const sql = "SELECT * FROM users WHERE username = ?";
-    db.query(sql, [username], (err, results) => {
-        console.log("Database query results:", results);
-        if (err) {
-            console.error('Database error:', err);
-            return res.status(500).send('Internal Server Error');
-        }
-
-        if (results.length > 0) {
-            if (results[0].password === password) {
-                res.json({ success: true, message: 'Login successful', username: username });
-            } else {
-                res.json({ success: false, message: 'Invalid username or password' });
-            }
-        } else {
-            res.json({ success: false, message: 'Invalid username or password' });
-        }
-    });
-});
-
 
 app.listen(3000, () => {
-    console.log('Server running on port 3000');
+  console.log("Server running on port 3000");
 });
