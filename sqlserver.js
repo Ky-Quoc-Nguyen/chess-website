18401ac52ea89844bbf9659eaa2c850133ca22f6
Ky Nguyen <kqn5tv@virginia.edu>
2024-01-20 19:34:31 -0500

    sdvsdv

--- sqlserver.js
+++ sqlserver.js
@@ -22,7 +22,11 @@ const config = {
     trustServerCertificate: false
   }
 };
+var options = {
 
+  index: 'hostingstart.html'
+  
+  };
 db.connect(config)
   .then(pool => {
     console.log('Connected to Azure SQL Database');
@@ -32,17 +36,19 @@ db.connect(config)
   });
 
   // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static('/home/site/wwwroot', options));
 
-// Send the user to index.html if the route is not recognized
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+// // Send the user to index.html if the route is not recognized
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public/index.html'));
+// });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
+app.listen(process.env.PORT);
+
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
